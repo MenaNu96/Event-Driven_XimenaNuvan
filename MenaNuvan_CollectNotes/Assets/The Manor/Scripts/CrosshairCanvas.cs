ef48fcd64be9a4640a7790bfb00ac76e77506ad2
MenaNu96 <151078182+MenaNu96@users.noreply.github.com>
2025-04-18 23:03:59 -0400

    update

---
+++ MenaNuvan_CollectNotes/Assets/The Manor/Scripts/CrosshairCanvas.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairCanvas : MonoBehaviour
+{
+    private RectTransform crosshair;
+    public bool interactable;
+    
+    void Start()
+    {
+        crosshair = transform.GetChild(0).GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        if (interactable == false) {
+            crosshair.sizeDelta = Vector2.Lerp(crosshair.sizeDelta, new Vector2(8, 8), Time.deltaTime * 4);
+        } else {
+            crosshair.sizeDelta = Vector2.Lerp(crosshair.sizeDelta, new Vector2(40, 40), Time.deltaTime * 4);
+        }
+    }
+}
